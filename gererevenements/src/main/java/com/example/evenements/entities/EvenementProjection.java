4035821f16483515cef716f0550418a1a0619ac3
marambelhajrhouma <marambhr47@gmail.com>
2024-11-08 00:43:06 +0100

    tp3

--- gererevenements/src/main/java/com/example/evenements/entities/EvenementProjection.java
+++
@@ -1,8 +0,0 @@
-package com.example.evenements.entities;
-
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name="nomEvenement", types= {Evenement.class})
-public interface EvenementProjection {
-	public String getNomEvenement();
-}
